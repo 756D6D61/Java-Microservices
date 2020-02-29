cd2f9757565ea55db70cfb760e8b7993ffd51301
756D6D61 <756D6D61@gmail.com>
2020-02-29 16:25:33 +0000

    HTTP headrs

--- src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -1,9 +1,14 @@
 package com.course.practicaljava.rest.controller;
 
 import java.time.LocalTime;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +26,30 @@ public class DefaultRestController {
 	@GetMapping("/time")
 	public String time() {
 		return LocalTime.now().toString();
+	}
+
+	@GetMapping("/header-one")
+	public String headerByAnnotation(@RequestHeader(name = "user-agent") String headerUserAgent,
+			@RequestHeader(name = "Practical-java", required = false) String headerPracticalJava) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("User-agent : " + headerUserAgent);
+		sb.append(" || ");
+		sb.append("Practical-java : " + headerPracticalJava);
+		return sb.toString();
+	}
+
+	@GetMapping("/header-two")
+	public String headerByServlet(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("User-agent : " + request.getHeader("User-agent"));
+		sb.append(" || ");
+		sb.append("Practical-java: " + request.getHeader("Practical-java"));
+		return sb.toString();
+	}
+
+	@GetMapping("/header-three")
+	public Map<String, String> headerByAll(@RequestHeader HttpHeaders headers) {
+		return headers.toSingleValueMap();
 	}
 
 }
