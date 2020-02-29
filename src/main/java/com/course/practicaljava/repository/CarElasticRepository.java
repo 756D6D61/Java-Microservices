d7b309b742cf7c1bf14e6e445ce488f0e172211f
756D6D61 <756D6D61@gmail.com>
2020-02-29 16:01:43 +0000

    Pagination

--- src/main/java/com/course/practicaljava/repository/CarElasticRepository.java
+++ src/main/java/com/course/practicaljava/repository/CarElasticRepository.java
@@ -1,10 +1,17 @@
 package com.course.practicaljava.repository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
-import com.course.practicaljava.rest.domain.car;
-@Repository
 
-public interface CarElasticRepository {
+import com.course.practicaljava.rest.domain.Car;
+
+@Repository
+public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
+	public List<Car> findByBrandAndColour(String brand, String colour);
+
+	@Query("{ \"range\": {\"first_release_date\":{ \"gt\" :? 0 } } })")
+	public List<Car> findByFirstReleaseDateAfter(long date);
 
 }
--- src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -1,19 +1,25 @@
 package com.course.practicaljava.rest.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.practicaljava.repository.CarElasticRepository;
 import com.course.practicaljava.rest.domain.Car;
 import com.course.practicaljava.rest.service.CarService;
 
@@ -48,6 +54,48 @@ public class CarRestController {
 		}
 
 		return result;
+	}
+
+	@GetMapping(path = "/cars/count")
+	public long countCar() {
+		return CarElasticRepository.count();
+	}
+
+	@PostMapping(path = "/cars", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Car createCar(@RequestBody Car car) {
+		return CarElasticRepository.save(car);
+	}
+
+	@GetMapping(path = "/cars/{id}")
+	public Car findCarsById(@PathVariable String id) {
+		return CarElasticRepository.findById(id).orElse(null);
+	}
+
+	@PutMapping(path = "/cars/{id}")
+	public Car updateCarById(@PathVariable String id, @RequestBody Car UpdatedCar) {
+		UpdatedCar.setId(id);
+		return CarElasticRepository.save(UpdatedCar);
+	}
+
+	@GetMapping(path = "/cars/{brand}/{color}")
+	public List<Car> findCarsByPath(@PathVariable String brand, @PathVariable String colour,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+		int pageable = PageRequest.of(page, size, Sortby(Direction.DESC, "price"));
+		return CarElasticRepository.findByBrandAndColour(brand, colour, pageable).getContent();
+	}
+
+	@GetMapping(path = "/cars")
+	public List<Car> findCarsByParam(@PathVariable String brand, @PathVariable String colour,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+		int pageable = PageRequest.of(page, size);
+		return CarElasticRepository.findByBrandAndColour(brand, colour, pageable).getContent();
+	}
+
+	@GetMapping(path = "/cars/date")
+	public List<Car> findCarsReleasedAfter(
+			@RequestParam(name = "first_realse_date") @DateTimeFormat(pattern = "yyy-MM-dd") Date firstReleaseDate) {
+		return CarElasticRepository.findByFirstReleaseDateAfter(firstReleaseDate.getTime());
+
 	}
 
 }
